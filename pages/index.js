afbd2ce6fddf59e148dec2d04caa1a625a287e33
lester-correya <lester.correya@vahvah.com>
2022-12-14 20:09:51 +0530

    skills section complete

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Header from '../components/Header'
 import HomeSection from '../components/HomeSection'
 import AboutSection from '../components/AboutSection'
+import SkillsSection from '../components/SkillsSection'
 
 export default function Home() {
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <HomeSection />
       <AboutSection />
+      <SkillsSection />
     </div >
   )
 }
