535adbce4b00192310791b1d17eefc1178614b56
lester-correya <lester.correya@vahvah.com>
2022-12-19 10:33:30 +0530

    Image lazy loading fixed

--- components/FixedBackground.js
+++ components/FixedBackground.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const FixedBackground = () => {
   return (
     <div className='fixed top-10 w-full -z-10 flex justify-center items-center md:justify-end lg:px-48'>
-      <Image src="/static/images/profile-image.jpg" width={500} height={780} alt="Profile Picture" className='rounded-full w-4/6 md:w-auto' />
+      <Image src="/static/images/profile-image.jpg" width={500} height={780} alt="Profile Picture" className='rounded-full w-4/6 md:w-auto' priority={true} />
     </div>
   )
 }
